04eb4a6d38ecdef169eacc3f30de7b59c7e21050
JoaoMoura01 <joaovictormoura131412@gmail.com>
2025-09-11 20:29:20 -0300

    Add files via upload

---
+++ atividade_joao_victor.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {       
+        String nome;
+    	int idade;
+    	double altura;
+    	boolean estudantes;
+    
+    	nome= "Isabela";
+    	idade= 16;
+    	altura= 1.65;
+    	estudantes= true;
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(estudantes);
+
+        int a;
+        int b;
+        int c;
+        int d;
+        a=25;
+        b=(a*3);
+        c=10;
+        d=a/c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+            
+    }
+}
