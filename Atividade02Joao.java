590bdd1c42941913db4f6bd517fbc680e6acfabd
JoaoMoura01 <joaovictormoura131412@gmail.com>
2025-09-16 21:05:05 -0300

    Add files via upload

---
+++ Atividade02Joao.java
@@ -0,0 +1,143 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ *
+ * @author Aluno
+ */
+public class JavaApplication1 {
+    public static void main(String[] args) {
+        //Atividade 1 - Circulo e raio:
+//       Scanner entrada = new Scanner(System.in);
+//       double raio,area,circuf;
+//       System.out.print("Digite o valor do raio do circulo:");
+//       raio = entrada.nextDouble();
+//       area = Math.PI*(Math.pow(raio,2));
+//       circuf = 2*Math.PI*raio;
+//       System.out.println("Resultado área:"+area);
+//       System.out.println("Resultado circunferência:"+circuf);
+//       
+       //Atividade 2 - IMC:
+      // Scanner entrada = new Scanner(System.in);
+      // double peso,altura,imc;
+      // System.out.print("Digite seu peso em kg: ");
+      // peso = entrada.nextDouble(); 
+       //System.out.print("Digite sua altura: ");
+      // altura = entrada.nextDouble(); 
+      // imc = peso/(altura*altura);
+      // System.out.println("Seu IMC: "+imc);
+       
+      //Atividade 3 - Hipotenusa:
+      //Scanner entrada = new Scanner(System.in);
+      //double cateto1,cateto2,triangulo,hipotenusa,n;
+      //System.out.print("Digite o primeiro cateto do triângulo retângulo: ");
+      //cateto1 = entrada.nextDouble ();
+      //System.out.print("Digite o segundo cateto do triângulo retângulo: ");
+      //cateto2 = ''.''''();
+      //hipotenusa = (Math.pow(cateto1,2))+(Math.pow(cateto2,2));
+      //n = Math.pow(hipotenusa,2);
+      //System.out.println("A hipotenusa é igual á: "+hipotenusa);
+      
+      //Atividade 4 - Dólar:
+//     Scanner entrada = new Scanner(System.in);
+//     double dolar,real,quantidade,inteiros, troco;
+//     System.out.print("Digite o preço do dolar: ");
+//     dolar = entrada.nextDouble ();
+//     System.out.print("Digite a quantia de reais: ");
+//     real = entrada.nextDouble ();
+//     quantidade = real/dolar;
+//     inteiros = Math.floor(quantidade);
+//     troco = (quantidade-inteiros)*dolar;
+//     System.out.println("A quantidade de dolares q podem ser comprados é: "+quantidade);
+//     System.out.println("O troco recebido é: "+troco);
+     
+     //Atividade 5 - Dado:
+//     Scanner entrada = new Scanner (System.in);
+//     int n,j1,j2,s;
+//     System.out.print("Digite o número da primeira jogada: ");
+//     j1 = entrada.nextInt();
+//     System.out.print("Digite o número da segunda jogada: ");
+//     j2 = entrada.nextInt();
+//     n = entrada.nextInt();
+//     s = (j1+j2)/n;
+//     System.out.println("A media é: "+s);
+//     System.out.println("O máximo é: "+ Math.max (j1,j2));
+//     System.out.println("O minímo é: " +Math.min(j1,j2));
+//     
+     
+     //Atividade 6 - Seno, cosseno, tangente:
+    // Scanner entrada = new Scanner (System.in);
+     //double angulo,seno,cosseno,tangente;
+     //System.out.print("Digite o angulo em graus:");
+     //angulo = entrada.nextDouble ();
+     //double anguloRadianos = Math.toRadians(angulo);
+     //seno = Math.sin(anguloRadianos);
+     //cosseno = Math.cos(anguloRadianos);
+     //tangente = Math.tan(anguloRadianos);
+     //System.out.println("Ângulo: " + angulo + " graus");
+     //System.out.println("Seno: " + seno);
+     //System.out.println("Cosseno: " + cosseno);
+     //System.out.println("Tangente: " + tangente);
+     
+     //Atividade 7 - Distância entre pontos:
+//     Scanner entrada = new Scanner (System.in);
+//     double x1,x2,y1,y2,cale
+//     x1 = entrada.nextDouble();
+//     x2 = "" "" "";
+//     y1 = "" "" "";
+//     y2 = "" "" "";
+//     calc=Math.sqrt(Math.pow(x2-x1),2))+
+//     (Math.pow(y2-y1),2));
+     
+      
+    //Atividade 8 - Operadores:
+//    Scanner entrada = new Scanner (System.in);
+//    double n1,n2,n3,md,dif,abs;
+//    System.out.print("Digite o primeiro número: ");
+//    n1 = entrada.nextDouble();
+//    System.out.print("Digite o segundo número: ");
+//    n2 = entrada.nextDouble ();
+//    System.out.print("Digite o terceiro número: ");
+//    n3= entrada.nextDouble();
+//    md = (n1+n2+n3)/3;
+//    System.out.print("A média é: "+md);
+//    abs = Math.abs(n1)+ Math.abs(n2) + Math.abs(n3);
+//    dif = Math.abs(n1-n3);
+//    System.out.print("\nTotal absoluto é: \n"+dif);
+
+   //Atividade 9 - Equação 2 grau
+//   Scanner entrada = new Scanner (System.in);
+//   int a,b,c;
+//   double x1,x2,delta;
+//   a = 2;
+//   b = -3;
+//   c = -2;
+//   delta = Math.pow(b,2)-(4*a*c);
+//   x1 = (-b)+Math.sqrt(delta)/2*2;
+//   x2 = (-b)-(Math.sqrt(delta))/(2*a);
+//   System.out.println("Resultado de delta: "+delta);
+//   System.out.println("Resultado de X1: "+ x1);
+//   System.out.println("Resultado de X2: "+ x2); 
+
+   //Atividade 10 - Conversão:
+//   Scanner entrada = new Scanner (System.in);
+//   double far,cel,calc;
+//   System.out.print("Digite a temperatura em Fahrenheit F°: ");
+//   far = entrada.nextDouble();
+//   cel = (far-32)/1.8;
+//   System.out.print("O valor de F° "+far+"em Fahrenheit é equivalente a C°"+cel);
+//     
+     
+     
+     
+     
+              
+    }
+    
+}
+
