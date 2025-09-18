01ac022605c596c8a3ca7e80f74f60fe1977336a
JoaoMoura01 <joaovictormoura131412@gmail.com>
2025-09-17 21:19:50 -0300

    Add files via upload

---
+++ Joaoatividade03.java
@@ -0,0 +1,185 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade03.isabela;
+import java.util.Scanner;
+import java.lang.Math;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade03Isabela {
+
+    public static void main(String[] args) {
+        //Atividade 1 - Gramas:
+//        Scanner entrada = new Scanner (System.in);
+//        double g,g1;  
+//        int porcao,porcao1;
+//        porcao = 8;
+//        g = 200;
+//        System.out.print("Quantidade deejada de porções:");
+//        porcao1 = entrada.nextInt();
+//        g1 = (porcao1*g)/porcao;
+//        System.out.println("A quantidade de açucar necessária é:"+g1);
+
+    //Atividade 2 - impressora:
+//    Scanner entrada = new Scanner (System.in);
+//    int imp,imp2,pag,min,min2,pmt,ppm;
+//    imp = 3;
+//    pag = 180;
+//    min = 12;
+//    System.out.print("Digite a quantidade de impressoras: ");
+//    imp2 = entrada.nextInt ();
+//    pmt = pag * imp2;
+//    min2 = pag/pmt;
+//    System.out.print("O total de minutos para impressora de 180 páginas com este numero de impressoras é:"+min2);
+
+//Atividade 3 - Consumo:
+//Scanner entrada = new Scanner (System.in);
+//double litros,l1,distancia,d1;
+//litros = 18.00;
+//distancia = 240.00;
+//System.out.print("Digite a distância percorrida: ");
+//d1 = entrada.nextDouble();
+//l1 = (d1*litros)/distancia;
+//System.out.println("A quantidade de litros necessários para "+d1+"é: "+l1);
+// 
+
+//Atividade 4 - Produção:
+//Scanner entrada = new Scanner (System.in);
+//int m,m1,h,h1,p,p1;
+//p = 900;
+//m = 6;
+//h = 5;
+//System.out.print("Digite a quantidade de maquinas:");
+//m1 = entrada.nextInt();
+//System.out.print("Digite o tempo em horas:");
+//h1 = entrada.nextInt();
+//p1 = (p*m1*h1) / (m*h);
+//System.out.println("A quantidade de peças produzidas em "+h1+" horas é "+p1+" por "+m1+" maquinas");
+
+//Atividade 5 - Conversão:
+//Scanner entrada = new Scanner (System.in);
+//double c,u,q;
+//int d;
+//System.out.print("Digite a cotação do dolar: ");
+//c = entrada.nextDouble();
+//System.out.print("Digite a quantia em doláres: ");
+//u = entrada.nextDouble();
+//q = u*c;
+//System.out.println("Para comprar "+u+" é necessário "+q);
+
+//Atividade 6 - Raiz de equação linear:
+//Scanner entrada = new Scanner (System.in);
+//double a,b,x,c;
+//System.out.print("Digite o valor de a: ");
+//a = entrada.nextDouble();
+//System.out.print("Digite o valor de b: ");
+//b = entrada.nextDouble();
+//x = -b/a;
+//c = a*x+b;
+//System.out.println("A é igual à: "+a);
+//System.out.println("B é igual à: "+b);
+//System.out.println("X é igual à: "+x);
+//System.out.println("C é igual à: "+c);
+
+//Atividade 7 - Função linear:
+//Scanner entrada = new Scanner (System.in);
+//int m,b,x0,y0;
+//System.out.print("Digite o M: ");
+//m = entrada.nextInt();
+//System.out.print("Digite o B: ");
+//b = entrada.nextInt();
+//System.out.print("Digite o X0: ");
+//x0 = entrada.nextInt();
+//y0 = m*x0+b;
+//System.out.println("O resultado é: "+y0);
+
+//Atividade 8 - Reta de dois pontos:
+//Scanner entrada = new Scanner (System.in);
+//int x1,y1,x2,y2,m,b,y;
+//System.out.print("Digite o valor de X1: ");
+//x1 = entrada.nextInt();
+//System.out.print("Digite o valor de Y1: ");
+//y1 = entrada.nextInt();
+//System.out.print("Digite o valor de X2: ");
+//x2 = entrada.nextInt();
+//System.out.print("Digite o valor de Y2: ");
+//y2 = entrada.nextInt();
+//m = (y2-y1)/(x2-x1);
+//b = y1 - m*x1;
+//System.out.print("y ="+m+"*x+"+b);
+     
+//Atividade 9 - Intersecção de duas retas:
+//Scanner entrada = new Scanner (System.in);
+//int m1,b1,m2,b2,x,y;
+//System.out.print("Digite M1: ");
+//m1 = entrada.nextInt();
+//System.out.print("Digite B1: ");
+//b1 = entrada.nextInt();
+//System.out.print("Digite M2: ");
+//m2 = entrada.nextInt();
+//System.out.print("Digite B2: ");
+//b2 = entrada.nextInt();
+//x = (b2-b1)/(m1-m2);
+//y = m1*x+b1;
+//System.out.println("O valor de X é "+x+" e o valor de Y é "+y);
+
+//Atividade 10 - Quadrática:
+//Scanner input = new Scanner(System.in);
+//double a,b,c,x1,x2,delta;
+//a = input.nextInt();
+//b = input.nextInt();
+//c = input.nextInt();
+//delta = (Math.pow(b,2)-(4*a*c));
+//x1 = (-b+ Math.sqrt (delta)/(2*a));
+//x2 = (-b-Math.sqrt(delta)/(2*a));
+//System.out.print("Valor de x1:"+x1);
+//""" ("Valor de x2:"+x1);
+
+//Atividade 11 - Distância:
+//double delta,a,d,c,b;
+// a = 5;
+// b = 2;
+// c = 1;
+//delta = (Math.pow(b,2)-(4*a*c));
+//d = Math.abs((Math.sqrt(delta))/a);
+//System.out.println("O resultado é: "+d);
+
+//Atividade 12 - Derivada:
+//Scanner entrada = new Scanner (System.in);
+//int a,b,c,x0;
+//double f,f1;
+//System.out.print("Digite o valor de A: ");
+//a = entrada.nextInt(0);
+//System.out.print("Digite o valor de B: ");
+//b = entrada.nextInt();
+//System.out.print("Digite o valor de C: ");
+//c = entrada.nextInt();
+//System.out.print("Digite o valor de X0: ");
+//x0 = entrada.nextInt();
+//f = a*x0^2+b*x0+c;
+//f1 = 2*a*x0+b;
+//System.out.println("f(x0)="+f+" e f'(x0)= "+f1);
+
+//Atividade 13 - Área: 
+//Scanner entrada = new Scanner (System.in);
+//double m, b, x1, x2, y1, y2, area;
+//System.out.print("Digite M: ");
+//m = entrada.nextDouble();
+//System.out.print("Digite B: ");
+//b = entrada.nextDouble();
+//System.out.print("Digite X1: ");
+//x1 = entrada.nextDouble();
+//System.out.print("Digite X2: ");
+//x2 = entrada.nextDouble();
+//y1 = m*x1+b;
+//y2 = m*x2+b;
+//area = ((y1+y2)/2)*(x2-x1);
+//System.out.println("A área é: "+area);
+//
+//    }
+//}
+
