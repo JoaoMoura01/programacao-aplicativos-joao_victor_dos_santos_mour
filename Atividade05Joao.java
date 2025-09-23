1cf9865b01622b5196d4515ca4e1521706d0905d
JoaoMoura01 <joaovictormoura131412@gmail.com>
2025-09-23 20:08:52 -0300

    Add files via upload

---
+++ Atividade05Joao.java
@@ -0,0 +1,210 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade05joao;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade05Joao {
+
+    public static void main(String[] args) {
+        /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+//        Atividade 1:
+//        int num = 0;
+//        int gradeCounter = 1;
+//        while (gradeCounter <=5){
+//        System.out.print("Digite um número: ");
+//        int numero = entrada.nextInt();
+//        num = num + numero;
+//        gradeCounter = gradeCounter + 1;
+//        }
+
+//        Atividade 2:
+//    int num = 0;
+//    System.out.print("Digite um número, digite -1 para encerrar: ");
+//    int numero = entrada.nextInt();
+//    int gradeCounter = 1;
+//    while (numero != -1) {
+//        num = num + numero;
+//        gradeCounter = gradeCounter + 1;
+//        System.out.print("Digite um número, digite -1 para encerrar: ");
+//        numero = entrada.nextInt();
+//    }
+//    System.out.println("\nTotal das somas é " + num + " e a quantidade de números é " + (gradeCounter - 1));
+
+//      Atividade 3:
+//        String senha = "";  
+//        System.out.print("Digite uma senha: ");
+//        senha = entrada.nextLine(); 
+//        String senhaCorreta = "java123"; 
+//
+//        while (!senha.equals(senhaCorreta)) {  
+//            System.out.println("Senha incorreta, tente novamente: ");
+//            senha = entrada.nextLine();
+//        }
+//        System.out.println("Senha Correta!");
+//    }
+//}
+
+//  Atividade 4:
+//    int opcao = 1,num1,num2,soma,sub,mult;
+//    while(opcao!=0){
+//        System.out.print("Menu");
+//        "" "" "" ("1-Somar");
+//        "" "" "" ("2-Subtrair");
+//        "" "" "" ("3-multiplicar");
+//        "" "" "" ("0-Sair");
+//        opcao = entrada.nextInt();
+//        Switch (opcao){
+//            case 1:
+//                System.out.print("Digite um numero: ");
+//                num1 = input.nextInt();
+//                System.out.print("Digite outro numero: ");
+//                num2 = entrada.nextInt();
+//                soma = num1+num2;
+//                System.out.print("A soma dos valores é:"+soma);
+//                break;
+//            case 2:
+//               System.out.print("Digite um numero: ");
+//                num1 = entrada.nextInt();
+//                System.out.print("Digite outro numero: ");
+//                num2 = entrada.nextInt();
+//                sub = num1-num2;
+//                System.out.print("A soma dos valores é:"+sub);
+//                break;
+//           case 3:
+//                System.out.print("Digite um numero: ");
+//                num1 = entrada.nextInt();
+//                System.out.print("Digite outro numero: ");
+//                num2 = entrada.nextInt();
+//                mult = num1*num2;
+//                System.out.print("A soma dos valores é:"+mult);
+//                break;
+//            case 0: 
+//                System.out.println("Saindo...");
+//                break;
+//
+//    }
+//    }
+
+//     Atividade 5:
+//       int n,f,i;
+//        System.out.print("Digite o valor de n: ");
+//        n = entrada.nextInt();
+//        i = 1;
+//        f = 1;
+//
+//        while(i<=n){
+//        f *= i;
+//        i++;
+//        System.out.println("O fatorial de "+n+" é "+f);
+//        }
+        
+    // Atividade 6:
+//    int n,f0,f1,proximo;
+//    System.out.print("Digite n: ");
+//    n = entrada.nextInt();
+//    f0 = 0;
+//    f1 = 1;
+//    proximo = f0 +f1;
+//    while(proximo<=n){
+//        System.out.println(","+proximo);
+//        f0=f1;
+//        f1=proximo;
+//        proximo = f0+f1; 
+//    }
+//   
+//     Atividade 7:
+//      int n;
+//        
+//        System.out.print("Digite n, para saber se ele é primo ou não primo! ");
+//        n = entrada.nextInt();
+//        
+//        if (n < 2) {
+//            System.out.println("Número inválido!");
+//        } else {
+//            boolean primo = true;
+//            int i = 2;
+//            
+//            while (i * i <= n) {
+//                if (n % i == 0) {
+//                    primo = false;
+//                    break;
+//                }
+//                i++;
+//            }
+//            
+//            if (primo) {
+//                System.out.println(n + " é um número primo!");
+//            } else {
+//                System.out.println(n + " é um número não primo!");
+//            }
+//        }
+//    }
+//}
+// Atividade 8:
+//    int n,num = 0;
+//    System.out.print("Digite um número, digite um número negativo caso queira parar: ");
+//    n = entrada.nextInt();
+//    num = num + n;
+//    while(n>=0){
+//        System.out.print("Digite outro número, digite um número negativo caso queira parar: ");
+//        n = entrada.nextInt();
+//        if (n>=0){
+//        num = num + n;
+//        }
+//    }
+//    System.out.println("Total das somas é " + num);
+//    }
+
+//Atividade 9:
+//    int n,soma = 0;
+//    System.out.print("Digite o número: ");
+//    n = entrada.nextInt();
+//    while (n>0){
+//        soma = soma +n;
+//        n--;
+//    }
+//    System.out.println("O valor é: "+soma);      
+//    
+//  Atividade 10: 
+//    int n;
+//    System.out.print("Digite o número: ");
+//    n = entrada.nextInt();
+//    if (n<0){
+//        System.out.println("Número inválido, digite um número maior que 0!");
+//        return;
+//    }
+//    while (n!=1){
+//        System.out.println(n);
+//            if (n % 2 == 0) {
+//                n = n / 2;
+//            } else {
+//                n = 3 * n + 1;
+//            }
+//        }
+//
+    }
+}
+    
+
+    
+    
+            
+        
+            
+       
+        
+    
+
+    }
+}
