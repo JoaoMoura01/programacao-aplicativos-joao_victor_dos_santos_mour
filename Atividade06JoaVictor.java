fedd8b16e9cdff8a9e9f3ed54c4b1b6cf31fe7c9
JoaoMoura01 <joaovictormoura131412@gmail.com>
2025-09-25 20:32:34 -0300

    Add files via upload

---
+++ Atividade06JoaVictor.java
@@ -0,0 +1,164 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.atividade06.joa.victor;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade06JoaVictor {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+//     Atividade  01:
+//       Double nota;
+//       do{
+//            System.out.print("Digite uma nota: ");
+//            nota = entrada.nextDouble();
+//       }
+//       while(nota > 10);{
+//            System.out.println("Nota válida!");
+//           
+//    }
+
+//      Atividadde 02:  
+//    double media;
+//    int num,soma=0,contador=0;
+//      do{
+//           System.out.print("Digite um número: ");
+//            num = entrada.nextInt();
+//     } while(num!=0);
+//      System.out.print("Soma:"+soma);
+//      System.out.print("Quantidade:"+contador);
+//      media=soma/contador;
+//      System.out.print("Média:"+media);
+      
+//       Atividade 03:
+//         int palp,tentativas =0;
+//         int num;
+//         num = (int) (Math.random() * 100) + 1;
+//         do{
+//             System.out.print("Digite um palpite: ");
+//             palp = entrada.nextInt();
+//             tentativas++;
+//             if(palp<num){
+//                    System.out.println("Palpite muito baixo! ");
+//             }
+//             else if(palp>num){
+//                    System.out.println("Palpite muito alto!");
+//             }
+//         }
+//         while(palp!=num);{
+//                System.out.println("Acertou em " + tentativas + " tentativas!");
+//                palp = entrada.nextInt();
+// }
+//   }
+
+//      Atividade 04
+//int n;
+//String op;
+//    do {
+//    do {
+//            System.out.print("Digite um número inteiro: ");
+//            n = entrada.nextInt();
+//            entrada.nextLine(); // limpar o buffer
+//            if (n <= 0) {
+//             System.out.println("Número inválido!");
+//            }
+//            } 
+//            while (n <= 0);
+//            for (int i = 0; i < n; i++) {
+//                System.out.println(n - i);
+//            }
+//            entrada.nextLine();
+//            System.out.print("Digite S para repetir ou N para sair: ");
+//            op = entrada.nextLine();
+//
+//        }    while (op.equalsIgnoreCase("S"));     
+//
+//    }
+//}
+
+// Atividade 05:
+//       String senha = "";  
+//       boolean acesso = false;
+//       int contador = 0;
+//       String senhaCorreta = "senai123";
+//      do{ 
+//          System.out.print("Digite uma senha: ");
+//            senha = entrada.nextLine(); 
+//            if (senha.equals(senhaCorreta)){
+//                acesso = true;
+//                break;
+//            }
+//            else{
+//                System.out.println("Senha incorreta, tente novamente: ");
+//                contador++;
+//            }          
+//      }
+//        while (contador < 3);
+//
+//                if (acesso) {
+//                    System.out.println("Senha correta! Acesso permitido.");
+//                } else {
+//                    System.out.println("Número máximo de tentativas excedido.");
+//                } 
+
+//          Atividade 06:
+//        int n,resultado;
+//        System.out.print("Digite um número: ");
+//        n = entrada.nextInt();
+//        for (int i = 1; i<= 10; i++);{
+//            resultado = n*i;
+//            System.out.println(n + " x " + i + " = " + resultado);
+//        }
+//     
+//   }
+//}
+
+//          Atividade 07:
+//    int n,somapar = 0,numero,somaimpar=0;
+//    String continuar;
+//    System.out.print("Digite quantos número você quer digitar: ");
+//    n=entrada.nextInt();
+//    for (int i =1; i<= n; i++){
+//        System.out.print("Digite o "+i+" número: ");
+//        numero=entrada.nextInt();
+//        if (n%2==0){
+//            somapar+=numero;
+//        }
+//        else{
+//            somaimpar+=numero;
+//        }
+//    }
+//    System.out.println("Soma dos números pares: "+somapar);
+//    System.out.println("Soma dos números impares: "+somaimpar);
+//    }
+//}
+
+//          Atividade 08:
+//    int n;
+//    System.out.print("Digite um número maior que 1: ");
+//    n = entrada.nextInt();
+//    if (n<=1){
+//       System.out.println("Sistema inválido!");
+//   }
+//    else{
+//        for (int i =2; i<=n;i++){
+//            boolean primo = true;
+//            for(int k =2;k*k<=i;k++){
+//            if(i%k == 0){
+//                primo =false;
+//                break;
+//            }
+//          }
+//        if (primo==true){
+//             System.out.print(i+",");   
+//      }
+//     }
+//    }
+//}
+    }
+}
